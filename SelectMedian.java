0459ced93739f29aa9d8295886f745213047142c
leozak06 <leoza.kabir@gmail.com>
2017-02-02 08:31:36 -0800

    Add files via upload

---
+++ SelectMedian.java
@@ -0,0 +1,22 @@
+package SelectMedian;
+
+/**
+ *
+ * @author Rahnuma Islam Nishat
+ * January 20, 2017
+ * CSC 226 - Spring 2017
+ */
+public class SelectMedian {
+
+       
+    public static int LinearSelect(int[] A, int k){
+        //Write your code here
+        return 0;
+    }    
+    
+    public static void main(String[] args) {
+        int[] A = {50, 54, 49, 49, 48, 49, 56, 52, 51, 52, 50, 59};
+        System.out.println("The median weight is " + LinearSelect(A, A.length/2));
+    }
+    
+}
